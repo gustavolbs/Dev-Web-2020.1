34cbd37fa7cde367d8ca33fd81de563117b40f20
Gustavo Santos <gustavo.santos@ccc.ufcg.edu.br>
2020-03-11 14:36:12 -0300

    feature: Added first route

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
+var axios = require("axios").default;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get("/", function(req, res, next) {
+//   res.send({ message: "respond with a resource" });
+// });
+
+// Get Everything
+router.get("/", async function(req, res, next) {
+  var response = {};
+
+  // Get currencies and cryptocoins
+  try {
+    const currencies = await axios.get(
+      "https://economia.awesomeapi.com.br/json/all"
+    );
+
+    response["currencies"] = currencies.data;
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Get Stocks
+  try {
+    const stocks = await axios.get("https://api.hgbrasil.com/finance");
+
+    response["stocks"] = stocks.data.results.stocks;
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.send(response);
 });
 
 module.exports = router;
